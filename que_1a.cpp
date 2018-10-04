373fef270d643cda6a955805b0b92805874ff274
Vatsal Soni <vatsal.soni@students.iiit.ac.in>
2018-10-04 16:43:29 +0530

    que_1a

---
+++ que_1a.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+struct suffix
+{
+    ll index;
+    ll child[2];
+};
+
+struct suffix *suffixarray;
+string input;
+
+ll comparator(struct suffix a, struct suffix b)
+{
+    if (a.child[0] < b.child[0])
+        return 1;
+    else if (a.child[0] > b.child[0])
+        return 0;
+    else
+    {
+        if (a.child[1] <= b.child[1])
+            return 1;
+        else
+            return 0;
+    }
+}
+
+void suffixSort()
+{
+    ll i, size, j, k;
+    size = input.length();
+    ll index[size];
+    for (i = 0; i < size; i++)
+    {
+        suffixarray[i].index = i;
+        suffixarray[i].child[0] = input[i] - '0';
+        if (i + 1 < size)
+        {
+            suffixarray[i].child[1] = input[i + 1] - '0';
+        }
+        else
+        {
+            suffixarray[i].child[1] = -1;
+        }
+    }
+    sort(suffixarray, suffixarray + size, comparator);
+    for (j = 4; j < 2 * size; j = j * 2)
+    {
+        index[suffixarray[0].index] = 0;
+        ll before = suffixarray[0].child[0];
+        ll current = 0;
+        suffixarray[0].child[0] = current;
+        for (k = 1; k < size; k++)
+        {
+            if (suffixarray[k].child[0] == before && suffixarray[k].child[1] == suffixarray[k - 1].child[1])
+            {
+                before = suffixarray[k].child[0];
+                suffixarray[k].child[0] = current;
+            }
+            else
+            {
+                before = suffixarray[k].child[0];
+                suffixarray[k].child[0] = current + 1;
+                current++;
+            }
+
+            index[suffixarray[k].index] = k;
+        }
+
+        for (k = 0; k < size; k++)
+        {
+            ll child_1 = suffixarray[k].child[0] + (j / 2);
+            if (child_1 < size)
+            {
+                ll in = index[child_1];
+                suffixarray[k].child[1] = suffixarray[in].child[0];
+            }
+            else
+            {
+                suffixarray[k].child[1] = -1;
+            }
+        }
+        sort(suffixarray, suffixarray + size, comparator);
+    }
+
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    ll i;
+    cin >> input;
+    suffixarray = new struct suffix[input.size()];
+    suffixSort();
+
+    for (i = 0; i < input.length(); i++)
+    {
+        cout << suffixarray[i].index << " ";
+    }
+    cout<<endl;
+    for(i=suffixarray[0].index;i<input.size();i++)
+    {
+        cout<<input[i];
+    }
+    for(i=0;i<suffixarray[0].index;i++)
+    {
+        cout<<input[i];
+    }
+    cout<<endl;
+    return 0;
+}
--- que_3.cpp
+++ que_3.cpp
@@ -1,69 +1,73 @@
-#include<bits/stdc++.h>
-#include<time.h>
+#include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-ll partition(vector<ll> &arr,ll low,ll high);
-void quicksort(vector<ll> &arr,ll low,ll high,ll k)
+ll partition(vector<ll> &arr, ll low, ll high);
+void find_nth(vector<ll> &arr, ll low, ll high, ll k)
 {
 	ll mid;
-	bool wantpos=false;
-	if(low<high)
+	bool wantpos = false;
+	if (low < high)
 	{
-		mid=partition(arr,low,high);
-		if(mid>k)
+		mid = partition(arr, low, high);
+		if (mid > k)
 		{
-			quicksort(arr,low,mid-1,k);
+			find_nth(arr, low, mid - 1, k);
 		}
-		else if(mid<k)
+		else if (mid < k)
 		{
-			quicksort(arr,mid+1,high,k);
+			find_nth(arr, mid + 1, high, k);
 		}
 	}
 }
-ll partition(vector<ll> &arr,ll low,ll high)
+ll partition(vector<ll> &arr, ll low, ll high)
 {
-	ll randpos=rand()%(high-low)+low;
-    ll i=low-1;
-  	ll tmp;
-  	tmp=arr[randpos];
-  	arr[randpos]=arr[high];
-  	arr[high]=tmp;
-  	ll pivot=arr[high];
-    for (ll j = low; j<=high-1; j++) 
-    { 
-        if (arr[j] <= pivot) 
-        { 
-        	i++;
-            tmp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=tmp; 
-        } 
-    }
-    tmp=arr[high];
-    arr[high]=arr[i+1];
-    arr[i+1]=tmp;
-    return (i + 1); 
+	ll randpos = rand() % (high - low) + low;
+	ll i = low - 1;
+	ll tmp;
+	tmp = arr[randpos];
+	arr[randpos] = arr[high];
+	arr[high] = tmp;
+	ll pivot = arr[high];
+	for (ll j = low; j <= high - 1; j++)
+	{
+		if (arr[j] <= pivot)
+		{
+			i++;
+			tmp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = tmp;
+		}
+	}
+	tmp = arr[high];
+	arr[high] = arr[i + 1];
+	arr[i + 1] = tmp;
+	return (i + 1);
 }
 int main()
 {
-	ll n,i,tmp,k;
-	cin>>n;
+	ios_base::sync_with_stdio(false);
+	ll n, i, tmp, k;
+	cin >> n;
+	cin >> k;
 	vector<ll> arr;
-	for(i=0;i<n;i++)
+	for (i = 0; i < n; i++)
 	{
-		cin>>tmp;
+		cin >> tmp;
 		arr.push_back(tmp);
 	}
-	vector<ll> v=arr;
-	while(1)
-	{
-		cin>>k;
-		k--;
-		quicksort(arr,0,arr.size()-1,k);
-		cout<<"nth="<<arr[k]<<endl;
-		nth_element(v.begin(),v.begin()+k,v.end());
-    	cout << "nth=" << v[k] << '\n';
-	}
+	vector<ll> v = arr;
+	k--;
+	clock_t start1 = clock();
+	find_nth(arr, 0, arr.size() - 1, k);
+	cout << "nth=" << arr[k] << endl;
+	clock_t end1 = clock();
+	clock_t start = clock();
+	nth_element(v.begin(), v.begin() + k, v.end());
+	cout << "nth=" << v[k] << endl;
+	clock_t end = clock();
+	cout << "mine=" << 1.0 * (end1 - start1) / CLOCKS_PER_SEC << endl;
+	cout << "org=" << 1.0 * (end - start) / CLOCKS_PER_SEC << endl;
+
 	// cout<<"SOrted"<<endl;
 	// for(i=0;i<arr.size();i++)
 	// 	cout<<arr[i]<<endl;
